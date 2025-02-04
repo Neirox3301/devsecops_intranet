2149a5bf9ce9c36266ce65c882493a2ecffb3954
frossetti <rossetti.fabcic3301@gmail.com>
2025-02-04 17:32:57 +0100

    PUTAIN LE DASHBOARD STUDENT FONCTIONNE!!!!!!!!

--- app/models/models.py
+++ app/models/models.py
@@ -56,7 +56,7 @@ class Class(db.Model):
     __tablename__ = 'classes'
 
     id = db.Column(db.Integer, primary_key=True)
-    class_name = db.Column(db.String(20), nullable=False)
+    name_class = db.Column(db.String(20), nullable=False)  # Correction ici
     head_teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=True)
 
     # Relations avec les autres tables
--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -7,24 +7,21 @@ from models import Grade, Class, Student
 student_dashboard_blueprint = Blueprint('student_dashboard', __name__)
 
 @student_dashboard_blueprint.route('/student_dashboard/grades')
-@login_required 
+@login_required
 def display_grades():
-    # Étape 1 : Récupérer l'étudiant lié à l'utilisateur connecté
     student = Student.query.filter_by(user_id=current_user.id).first()
 
     if not student:
         return redirect(url_for('student_dashboard.no_grades'))
 
-    # Étape 2 : Récupérer les notes de l'étudiant avec les matières associées en une seule requête
+    # Récupération des notes et des matières associées
     student_grades = Grade.query.options(joinedload(Grade.subject)).filter_by(student_id=student.id).all()
 
     if not student_grades:
         return redirect(url_for('student_dashboard.no_grades'))
 
-    # Étape 3 : Créer une liste des matières et des notes
     subjects = [{'id': grade.subject.id, 'name': grade.subject.name, 'grade': grade.grade} for grade in student_grades]
 
-    # Étape 4 : Récupérer les informations sur la classe de l'étudiant
     student_class = Class.query.filter_by(id=student.class_id).first()
 
     return render_template('student_templates/student_grades.html', subjects=subjects, student_class=student_class, student=student)
@@ -35,20 +32,24 @@ def display_grades():
 def display_bulletins():
     return render_template('student_templates/student_bulletins.html')
 
+
 @student_dashboard_blueprint.route('/student_dashboard/messagerie')
 @login_required
 def display_messagerie():
     return render_template('student_templates/student_messagerie.html')
+
 
 @student_dashboard_blueprint.route('/student_dashboard/professeurs')
 @login_required
 def display_professeurs():
     return render_template('student_templates/student_professeurs.html')
 
+
 @student_dashboard_blueprint.route('/student_dashboard/parametres')
 @login_required
 def display_parametres():
     return render_template('student_templates/student_parametres.html')
+
 
 # Fonction pour gérer l'absence de notes
 @student_dashboard_blueprint.route('/student_dashboard/no_grades')
